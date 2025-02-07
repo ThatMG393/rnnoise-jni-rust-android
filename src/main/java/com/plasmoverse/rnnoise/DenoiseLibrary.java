e4df596846c6348665b1702e0208bf22e1195235
thatmidcoder393 <73651803+ThatMG393@users.noreply.github.com>
2025-02-07 09:13:10 +0800

    Fix build

--- src/main/java/com/plasmoverse/rnnoise/DenoiseLibrary.java
+++ src/main/java/com/plasmoverse/rnnoise/DenoiseLibrary.java
@@ -83,7 +83,7 @@ public final class DenoiseLibrary {
             case "aarch64":
                 return "aarch64";
             default:
-                return OS_ARCH;
+                return systemArch;
         }
     }
 
@@ -96,7 +96,7 @@ public final class DenoiseLibrary {
             case "windows":
                 return library + ".dll";
             default:
-                throw new IllegalStateException("System is not supported: " + systemName);
+                throw new IllegalStateException("System is not supported: " + getPlatformName());
         }
     }
 
